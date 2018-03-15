7b77044853f626d47b9c57ad4fdaacd87da158cb
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-16 00:56:33 +0700

    creating a page and navigate to it

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { UsersPage } from '../users/users';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToUsers() {
+    this.navCtrl.push(UsersPage)
   }
 
 }
