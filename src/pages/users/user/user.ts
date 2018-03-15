88f29b18e4017b4f09a634bd537beb057ae6b61a
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-16 01:37:30 +0700

    popping pages - going back

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-user',
@@ -8,10 +8,16 @@ import { NavParams } from 'ionic-angular';
 export class UserPage implements OnInit {
   name: string;
 
-  constructor  (private navParams: NavParams) {}
+  constructor  (private navParams: NavParams,
+                private navCtrl: NavController) {}
 
   ngOnInit() {
     this.name = this.navParams.get('userName');
+  }
+
+  onGoBack() {
+    // this.navCtrl.pop();
+    this.navCtrl.popToRoot();
   }
 
 }
